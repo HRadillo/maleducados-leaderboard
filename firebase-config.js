f03609ba5d02c6abf25bd03bc57180bdbb5479e4
HRadillo <113868891+HRadillo@users.noreply.github.com>
2026-05-05 21:53:35 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 window.MALEDucadosFirebaseConfig = {
-  enabled: false,
+  enabled: true,
   adminEmail: "losmaleducadosdelmagic@gmail.com",
   collectionName: "site",
   documentId: "leaderboard",
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDK7Ez7E-3QVFPDWMFrANlQfW2Nc5ZTIhM",
+    authDomain: "maleducados-leaderboard.firebaseapp.com",
+    projectId: "maleducados-leaderboard",
+    storageBucket: "maleducados-leaderboard.firebasestorage.app",
+    messagingSenderId: "460330615641",
+    appId: "1:460330615641:web:3bb77eafb4409e0c500248"
   }
 };
